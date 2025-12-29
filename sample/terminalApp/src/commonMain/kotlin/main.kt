ede661c41aa1443d8e6aeff2aa039546ff2482a4
tbsten <programmingcafeteria@gmail.com>
2025-12-30 06:42:36 +0900

    init

---
+++ sample/terminalApp/src/commonMain/kotlin/main.kt
@@ -0,0 +1,5 @@
+import me.tbsten.kotlin.tuple.getFibonacciNumbers
+
+fun main() {
+    println("getFibonacciNumbers(7)=${getFibonacciNumbers(7).joinToString(", ")}")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+rootProject.name = "kotlin-tuple"
+
+pluginManagement {
+    repositories {
+        google {
+            content { 
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            content { 
+              	includeGroupByRegex("com\\.android.*")
+              	includeGroupByRegex("com\\.google.*")
+              	includeGroupByRegex("androidx.*")
+              	includeGroupByRegex("android.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+include(":kotlin-tuple")
+include(":sample:composeApp")
+include(":sample:terminalApp")
+
